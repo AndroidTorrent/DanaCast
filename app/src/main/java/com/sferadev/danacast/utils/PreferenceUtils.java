8842e9e1277418ffdaa0647131e92cfd776d0eaa
Alexis Rico <me@sferadev.com>
2015-09-25 19:48:24 +0200

    Dana: Initial History implementation through GSON

--- app/src/main/java/com/sferadev/danacast/utils/PreferenceUtils.java
+++ app/src/main/java/com/sferadev/danacast/utils/PreferenceUtils.java
@@ -4,10 +4,20 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.sferadev.danacast.models.EntryModel;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class PreferenceUtils {
     public static final String PROPERTY_PORDEDE_USER = "pordedeUser";
     public static final String PROPERTY_PORDEDE_PASS = "pordedePass";
     public static final String PROPERTY_LAST_PROVIDER = "lastProvider";
+    public static final String PROPERTY_HISTORY = "historyArray";
 
     public static String getPreference(Context context, String key, String defaultValue) {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
@@ -38,5 +48,18 @@ public class PreferenceUtils {
         SharedPreferences.Editor editor = settings.edit();
         editor.remove(preference);
         editor.apply();
+    }
+
+    public static String entryToJSON(ArrayList<EntryModel> arrayList) {
+        return new Gson().toJson(arrayList);
+    }
+
+    public static ArrayList<EntryModel> entryFromJSON(String json, boolean reverse) {
+        if (json == null) return new ArrayList<>();
+        Type type = new TypeToken<List<EntryModel>>() {
+        }.getType();
+        ArrayList<EntryModel> result = new Gson().fromJson(json, type);
+        if (reverse) Collections.reverse(result);
+        return result;
     }
 }
