cb8496e67e2da3f793b26a8a12c37bee23a51e37
Alexis Rico <me@sferadev.com>
2015-11-16 19:14:58 +0100

    Dana: Add Vidgg server

--- app/src/main/java/com/sferadev/danacast/helpers/Server.java
+++ app/src/main/java/com/sferadev/danacast/helpers/Server.java
@@ -2,11 +2,13 @@ package com.sferadev.danacast.helpers;
 
 import com.sferadev.danacast.servers.Nowvideo;
 import com.sferadev.danacast.servers.Streamcloud;
+import com.sferadev.danacast.servers.Vidgg;
 
 public class Server {
     public static boolean isSupported(String url) {
         return url.contains("streamcloud") ||
                 url.contains("nowvideo") ||
+                url.contains("vidgg") ||
                 url.contains("jkmedia") ||
                 url.contains("music") ||
                 url.contains("soundcloud") ||
@@ -24,6 +26,8 @@ public class Server {
                 return Streamcloud.getVideoPath(url);
             } else if (url.contains("nowvideo")) {
                 return Nowvideo.getVideoPath(url);
+            } else if (url.contains("vidgg")) {
+                return Vidgg.getVideoPath(url);
             } else return url;
         } catch (NullPointerException e) {
             e.printStackTrace();
---
+++ app/src/main/java/com/sferadev/danacast/servers/Vidgg.java
@@ -0,0 +1,32 @@
+package com.sferadev.danacast.servers;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Vidgg {
+    public static String getVideoPath(final String url) {
+        final String[] result = {null};
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String id = url.substring(url.lastIndexOf("/") + 1);
+                    Document doc = Jsoup.connect("http://www.vidgg.to/mobile/video.php?id=" + id)
+                            .timeout(3000)
+                            .get();
+                    result[0] = doc.select("source[type=video/mp4]").attr("src");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+            return result[0];
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
