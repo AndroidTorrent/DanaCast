6a53b2c5d0db2b78c19ccb0d8a0c1a7f03552186
Alexis Rico <me@sferadev.com>
2015-11-16 18:54:08 +0100

    Dana: Remove SeriesBlanco for now

--- app/src/main/java/com/sferadev/danacast/helpers/Constants.java
+++ app/src/main/java/com/sferadev/danacast/helpers/Constants.java
@@ -26,16 +26,15 @@ public class Constants {
     public static final int ABOUT_GOOGLE_PLUS = 0;
     public static final int ABOUT_WEBSITE = 1;
 
-    public static final int PROVIDER_SERIESBLANCO = 0;
-    public static final int PROVIDER_SERIESYONKIS = 1;
-    public static final int PROVIDER_WATCHSERIES = 2;
-    public static final int PROVIDER_PORDEDE_SHOWS = 3;
-    public static final int PROVIDER_GNULA = 4;
-    public static final int PROVIDER_ZPELICULAS = 5;
-    public static final int PROVIDER_PORDEDE_MOVIES = 6;
-    public static final int PROVIDER_JKANIME = 7;
-    public static final int PROVIDER_MUSIC163 = 8;
-    public static final int PROVIDER_SOUNDCLOUD = 9;
+    public static final int PROVIDER_SERIESYONKIS = 0;
+    public static final int PROVIDER_WATCHSERIES = 1;
+    public static final int PROVIDER_PORDEDE_SHOWS = 2;
+    public static final int PROVIDER_GNULA = 3;
+    public static final int PROVIDER_ZPELICULAS = 4;
+    public static final int PROVIDER_PORDEDE_MOVIES = 5;
+    public static final int PROVIDER_JKANIME = 6;
+    public static final int PROVIDER_MUSIC163 = 7;
+    public static final int PROVIDER_SOUNDCLOUD = 8;
 
     public static final int READ_EXTERNAL_STORAGE = 0;
     public static final int WRITE_EXTERNAL_STORAGE = 1;
